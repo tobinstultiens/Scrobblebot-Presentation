8cd9b17f2c13674452b2ee0aa215a69aa25fcb92
Tobin Stultiens <tobin.stultiens@gmail.com>
2019-12-10 14:21:16 +0100

    created a first setup with mock data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div justify-content-center id="app">
     <v-app id="title">
-      <v-h1 class="display-2 text-center">Scrobblebot</v-h1>
-      <router-view />
+      <v-h1 class="display-2 text-center mt-2">Scrobblebot</v-h1>
+      <v-content>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view />
+        </v-container>
+      </v-content>
+      <SideBar />
     </v-app>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import SideBar from "@/components/SideBar.vue";
 
-<style>
-</style>
+export default {
+  name: "app",
+  components: {
+    SideBar
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <SideBar />
     <CommandComponent msg="kick1999: .fm" />
   </div>
 </template>
@@ -8,13 +7,11 @@
 <script>
 // @ is an alias to /src
 import CommandComponent from "@/components/CommandComponent.vue";
-import SideBar from "@/components/SideBar.vue";
 
 export default {
   name: "home",
   components: {
-    CommandComponent,
-    SideBar
+    CommandComponent
   }
 };
 </script>
